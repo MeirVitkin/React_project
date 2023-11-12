be9c4b384e74a6515b2e2d1dc91445fbea605e54
meirVitkin <vitkin4580@gmail.com>
2023-11-12 13:02:56 +0200

    i adde dspace button

--- src/component/keyboard.js
+++ src/component/keyboard.js
@@ -6,17 +6,25 @@ function Keyboard(){
     const keyes = [
         'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
         'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-        'Z', 'X', 'C', 'V', 'B', 'N', 'M',' '
+        'Z', 'X', 'C', 'V', 'B', 'N', 'M',
     ];
     return(
         <div className="keyboard">
             {keyes.map((key,index)=>{
                 return(
+                   
+                    
                     <div className="key" key={index} onClick={showLeter}>
                         {key}
                     </div>
+                    
+                   
+
                 )
             })}
+            <div className ="space">space</div>
+            
+
         </div>
     )
 
